82b726d1a45453c2a2657a85c644ed7cb3449779
Chad Smith <csmith@Chads-MacBook-Pro.local>
2022-01-06 22:33:24 -0700

    removed vpn

--- global/_outputs.tf
+++ global/_outputs.tf
@@ -1,28 +1,4 @@
-output "nearam_vpn" {
-  value       = var.nearam_vpn
-  description = "Near Americas OpenVPN Endpoints"
-}
-
-output "nearam_vpn_east_vpcid" {
-  value = var.nearam_vpn_east_vpcid
-}
-
-output "nearam_vpn_east_rtid" {
-  value = var.nearam_vpn_east_rtid
-}
-
-output "nearam_vpn_east_cidr" {
-  value = var.nearam_vpn_east_cidr
-}
-
-output "nearam_vpn_west_vpcid" {
-  value = var.nearam_vpn_west_vpcid
-}
-
-output "nearam_vpn_west_rtid" {
-  value = var.nearam_vpn_west_rtid
-}
-
-output "nearam_vpn_west_cidr" {
-  value = var.nearam_vpn_west_cidr
+output "brightkey_vpn" {
+  value       = var.brightkey_vpn
+  description = "Brightkey OpenVPN Endpoints"
 }
--- global/_variables.tf
+++ global/_variables.tf
@@ -1,31 +1,4 @@
-variable "nearam_vpn" {
-  description = "Near Americas OpenVPN Endpoints"
-  default     = ["52.205.18.48/32", "52.39.103.57/32", "10.255.3.103/32", "10.254.1.119/32"]
+variable "brightkey_vpn" {
+  description = "Brightkey OpenVPN Endpoints"
+  default     = ["1.2.3.4/32", "5.6.7.8/32"]
 }
-
-variable "nearam_vpn_east_vpcid" {
-  default = "vpc-06d5abd9137a1fcda"
-  type = string
-}
-
-variable "nearam_vpn_east_rtid" {
-  default = ["rtb-0f81f92bb5e9225fb"]
-}
-
-variable "nearam_vpn_east_cidr" {
-  default = "10.255.0.0/16"
-}
-
-variable "nearam_vpn_west_vpcid" {
-  default = "vpc-0bd6b4e4eb4c11e95"
-  type = string
-}
-
-variable "nearam_vpn_west_rtid" {
-  default = ["rtb-034701fff93eea17e"]
-}
-
-variable "nearam_vpn_west_cidr" {
-  default = "10.254.0.0/16"
-}
-
